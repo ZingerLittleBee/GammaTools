91bb3cebde27c303e888e5d1d2fb80995cc4526e
bee <404846146@qq.com>
2021-01-25 20:44:38 +0800

    add d.ts

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+
+declare function randomBoolean(): boolean;
+
+declare function isWeekday(date: Date): boolean
+
+declare function reverse(str: string): string
+
+declare function isEven(num: number): boolean
+
+declare function timeFromDate(date: Date): string
+
+declare function toFixed(n: number, fixed: number)
+
+declare function isAppleDevice()
+
+declare function goToTop()
+
+declare function average(...args: number[]): number
+
+declare function celsiusToFahrenheit(celsius: number): number
+
+declare function fahrenheitToCelsius(fahrenheit: number)
